6be6d1f947cd4ddecfe9a7b22bd55425f28eba19
JuanOteroOK <juaan.otero@hotmail.com>
2021-07-11 16:40:19 -0300

    formversionviejaerrorvalidar

--- Frontend/src/app/pages/signin/signin.component.ts
+++ Frontend/src/app/pages/signin/signin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 
@@ -12,21 +12,99 @@ export class SigninComponent implements OnInit {
 
  step: any = 1;
  
-  constructor() { 
-   
-  }
+ formsign: FormGroup;
+
+ constructor(private formBuilder: FormBuilder) {
+  this.formsign= this.formBuilder.group(
+    {
+      password:['',[Validators.required, Validators.minLength(8)]],
+      email:['', [Validators.required, Validators.email]],
+      name:['', [Validators.required, Validators.email]],   
+      lastname:['', [Validators.required, Validators.email]], 
+      username:['', [Validators.required, Validators.email]],   
+      cpassword:['', [Validators.required, Validators.minLength(8)]]  
+    }
+  )
+
+ }
 
   ngOnInit(): void {
   }
 
-  siguiente(){
+  next(){
     this.step  = this.step + 1;
   }
   anterior(){
     this.step = this.step - 1;
   }
   confirmado(){
-  
+  this.step = 4;
   
   }
+
+  get nameField(){
+      return this.formsign.get("name");
+  }
+
+  get lastnameField(){
+      return this.formsign.get("lastname");
+  }
+  get usernameField(){
+      return this.formsign.get("username");
+  }
+  get emailField(){
+      return this.formsign.get("email");
+  }
+  get passwordField(){
+      return this.formsign.get("password");
+  }
+  get cpasswordField(){
+      return this.formsign.get("cpassword");
+  }
+
+  get nameInvalid()
+{
+return this.formsign.touched && !this.formsign.valid;
 }
+
+get lastnameInvalid()
+{
+return this.formsign.touched && !this.formsign.valid;
+}
+
+get usernameInvalid()
+{
+return this.formsign.touched && !this.formsign.valid;
+}
+
+get emailInvalid()
+{
+return this.formsign.touched && !this.formsign.valid;
+}
+
+get passwordInvalid()
+{
+return this.formsign.touched && !this.formsign.valid;
+}
+
+get cpasswordInvalid()
+{
+return this.formsign.touched && !this.formsign.valid;
+}
+
+
+ onSiguiente(event: Event)
+  {
+    event.preventDefault(); //Cancela la funcionalidad por default.
+    if (this.formsign.valid)
+    {
+      console.log(this.formsign.value); //se puede enviar al servidor...
+    }
+    else
+    {
+      this.formsign.markAllAsTouched(); //Activa todas las validaciones
+    }
+  }
+
+}
+
