d58f2fb60fdaca27902244740868571a799fff0e
JuanOteroOK <juaan.otero@hotmail.com>
2021-07-17 22:23:25 -0300

    creaciondelarama

---
+++ Frontend/src/app/pages/wallet/wallet.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppRoutingModule
+  ],
+  exports:[]
+})
+export class WalletModule { }
