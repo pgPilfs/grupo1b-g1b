37925def9435ea1f301000210ecec718718726ee
JuanOteroOK <juaan.otero@hotmail.com>
2021-09-13 19:30:02 -0300

    schemapages

--- Frontend/src/app/pages/pages.module.ts
+++ Frontend/src/app/pages/pages.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { HomeComponent } from './home/home.component';
